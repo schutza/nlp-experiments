4316bc9807ce7cf34eb7cb7b0ec394b24540c76b
Alexander Schutz <alexander.schutz@textability.com>
2020-02-23 17:13:49 +0000

    add dialog-acts affirmation

---
+++ regular-expressions/language_resources.py
@@ -0,0 +1,7 @@
+YES_WORD_NON_NEGATABLE = r'\b(yes|yup|sure|yeah)\b'
+YES_WORD_NEGATABLE_RHS = r'\b(certainly|absolutely)\b'
+YES_WORD_NEGATABLE_LHS = r'\b(right|okay)\b'
+
+NO_WORD_NON_NEGATABLE = r'\b(no|nope|incorrect|nah)\b'
+
+NEGATION_WORD = r'\b(not|hardly)\b'
