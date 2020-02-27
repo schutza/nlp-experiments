fd12bff8a475a884fdc3d269d17931eba421b94d
Alexander Schutz <alexander.schutz@textability.com>
2020-02-27 22:51:59 +0000

    refactor to classes

--- dialog-acts/src/tests/test_affirmations.py
+++ dialog-acts/src/tests/test_affirmations.py
@@ -1,50 +1,58 @@
 import pytest
 
-from dialog_acts.affirmations import is_affirm
+from dialog_acts.affirmations import RegexAffirmationDetector
 
-yes_phrase_testdata = [
-    ("that's right", True),
-    ("okay", True),
-    ("ok", True),
-    ("yes", True),
-    ("for sure", True),
-    ("right", True),
-    ("correct", True),
-    ("absolutely", True),
-    ("yep", True),
-    ("yeah", True),
-    ("certainly", True),
-    ("i want it", True),
-    ("yup", True),
-    ("yessir", True),
-    ("affirmative", True),
-    ("true", True)
-]
+class TestRegexAffirmationDetector:
 
-@pytest.mark.parametrize("input_phrase,expected", yes_phrase_testdata)
-def test_is_affirm_with_yes_phrases(input_phrase, expected):
-    actual = is_affirm(input_phrase)
-    assert actual[0] == expected
+    def setup_method(self, test_method):
+        self.detector = RegexAffirmationDetector()
 
-no_phrase_testdata = [
-    ("not right", False),
-    ("not correct", False),
-    ("not true", False),
-    ("absolutely not", False),
-    ("certainly not", False)
-]
+    def teardown_method(self, test_method):
+        self.detector = None
 
-@pytest.mark.parametrize("input_phrase,expected", no_phrase_testdata)
-def test_is_affirm_no_phrases(input_phrase, expected):
-    actual = is_affirm(input_phrase)
-    assert actual[0] == expected
+    yes_phrase_testdata = [
+        ("that's right", True),
+        ("okay", True),
+        ("ok", True),
+        ("yes", True),
+        ("for sure", True),
+        ("right", True),
+        ("correct", True),
+        ("absolutely", True),
+        ("yep", True),
+        ("yeah", True),
+        ("certainly", True),
+        ("i want it", True),
+        ("yup", True),
+        ("yessir", True),
+        ("affirmative", True),
+        ("true", True)
+    ]
 
-nonsense_testdata = [
-    ("foo bar", False),
-    ("yadda yadda", False)
-]
+    @pytest.mark.parametrize("input_phrase,expected", yes_phrase_testdata)
+    def test_is_affirm_with_yes_phrases(self, input_phrase, expected):
+        actual = self.detector.detect(input_phrase)
+        assert actual[0] == expected
 
-@pytest.mark.parametrize("input_phrase,expected", no_phrase_testdata)
-def test_is_affirm_nonsense_phrases(input_phrase, expected):
-    actual = is_affirm(input_phrase)
-    assert actual[0] == expected
+    no_phrase_testdata = [
+        ("not right", False),
+        ("not correct", False),
+        ("not true", False),
+        ("absolutely not", False),
+        ("certainly not", False)
+    ]
+
+    @pytest.mark.parametrize("input_phrase,expected", no_phrase_testdata)
+    def test_is_affirm_no_phrases(self, input_phrase, expected):
+        actual = self.detector.detect(input_phrase)
+        assert actual[0] == expected
+
+    nonsense_testdata = [
+        ("foo bar", False),
+        ("yadda yadda", False)
+    ]
+
+    @pytest.mark.parametrize("input_phrase,expected", no_phrase_testdata)
+    def test_is_affirm_nonsense_phrases(self, input_phrase, expected):
+        actual = self.detector.detect(input_phrase)
+        assert actual[0] == expected
