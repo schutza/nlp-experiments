47a85325fbdfe1023d50ece23e29dfe9e6094bbf
Alexander Schutz <alexander.schutz@textability.com>
2020-03-10 23:46:45 +0000

    remove dialog-acts

--- dialog-acts/README.md
+++
@@ -1,9 +0,0 @@
-# Dialog Acts
-
-A library for the detection of dialogue acts.
-
-## Testing
-
-Running unit tests using `pytest` by executing
-
-    pytest
--- dialog-acts/src/textability/dialog_acts/base_detectors.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-class DialogActDetector(ABC):
-    
-    @abstractmethod
-    def detect(self, input, language):
-        pass
